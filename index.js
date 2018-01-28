59440e9293d247979459faffb0cc96af8f8ef9ff
Howard Atkins <atkins.howard@gmail.com>
2018-01-28 17:15:36 +0000

    Initial version of javascript npm test

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+import moment from 'moment';
+
+console.log("Hello from Javascript! Latest");
+console.log(moment().startOf('day').fromNow());
+
+var name="Howard", time = "today";
+console.log(`Hello ${name}, how are you doing: ${time}?!!!`);
+
