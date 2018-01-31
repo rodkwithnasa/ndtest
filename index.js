f6ff23ea3c976f2032809964e369e0a7d33f2e9d
Howard Atkins <atkins.howard@gmail.com>
2018-01-31 18:50:57 +0000

    update to add call to statuses package

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // index.js
 
 import moment from 'moment';
+import status from 'statuses';
 
 console.log("Hello from Javascript! Latest");
 console.log(moment().startOf('day').fromNow());
 
 var name="Howard", time = "today";
+var code=status('forbidden');
+var msg=status[404];
 console.log(`Hello ${name}, how are you doing: ${time}?!!!`);
+console.log(`Errorcode is ${code}, errormsg is ${msg}.` );
 
